16663f7e5c76834c64af70a405db43b4051f6afe
Menchu MV <mcmvillalobos@gmail.com>
2021-11-03 19:09:58 +0100

    Set up html with function results

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ const port = process.env.SERVER_CALCULATOR_TERMINATOR || 5000;
 
 server.listen(port);
 
+const generateHTML = ({
+  num1,
+  num2,
+  sum,
+  substraction,
+  multiplication,
+  division,
+}) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <title>Calculadora 🐟</title>
+</head>
+<body>
+  <h1>Calculadora 🐟</h1>
+  <div>Resultados:</div>
+  <ul>
+    <li>${num1} + ${num2} = ${sum}</li>
+    <li>${num1} - ${num2} = ${substraction}</li>
+    <li>${num1} * ${num2} = ${multiplication}</li>
+    <li>${num1} / ${num2} = ${division}</li>
+  </ul>
+</body>
+</html>`;
+
 server.on("request", (request, response) => {
   const getParams = new URL(request.url, `htpp://${request.headers.host}`);
   const values = getParams.searchParams.values();
@@ -18,8 +46,6 @@ server.on("request", (request, response) => {
   }
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/html");
-  response.write("<h1>Hola Peix</h1>");
-
-  // response.setHeader("Content-Type", "application/json");
+  response.write(generateHTML(operation(params[0], params[1])));
   response.end();
 });
