458a4bc1fb02f05959fe52e2de453ef54282d73f
Menchu MV <mcmvillalobos@gmail.com>
2021-11-03 17:55:54 +0100

    Created port

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ require("dotenv").config();
 const http = require("http");
 
 const server = http.createServer();
+
+const port = process.env.SERVER_CALCULATOR_TERMINATOR || 5000;
+
+server.listen(port);
